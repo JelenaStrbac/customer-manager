2d34ef0b9746080d931b7d27619f2cd8d08e7c8a
apple juice <strbac.jelena.js@gmail.com>
2020-07-04 00:45:18 +0200

    [f]: basic structure

---
+++ src/components/Customers/CustomerCreate/CustomerCreate.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./CustomerCreate.css";
+
+const CustomerCreate = () => {
+  return <div className="CustomerCreate">CustomerCreate</div>;
+};
+
+export default CustomerCreate;
---
+++ src/components/Customers/CustomersList/CustomersList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./CustomersList.css";
+
+const CustomersList = () => {
+  return <div className="CustomersList">CustomersList</div>;
+};
+
+export default CustomersList;
