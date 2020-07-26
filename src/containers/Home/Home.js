fa29135c13b1497c0f65f25497a2970232a52b13
apple juice <strbac.jelena.js@gmail.com>
2020-07-26 13:55:31 +0200

    [fix]: removed cust.delete

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -6,7 +6,6 @@ import Toolbar from "../Toolbar/Toolbar";
 import CustomersList from "../Customers/CustomersList/CustomersList";
 import CustomerCreate from "../Customers/CustomerCreate/CustomerCreate";
 import CustomerEdit from "../Customers/CustomerEdit/CustomerEdit";
-// import CustomerDelete from "../Customers/CustomerDelete/CustomerDelete";
 import CustomerShow from "../Customers/CustomerShow/CustomerShow";
 import Navbar from "../Navbar/Navbar";
 import Documentation from "../../components/Documentation/Documentation";
@@ -21,7 +20,6 @@ const Home = () => {
           <Route path="/" exact component={CustomersList} />
           <Route path="/new" exact component={CustomerCreate} />
           <Route path="/edit/:id" exact component={CustomerEdit} />
-          {/* <Route path="/delete/:id" exact component={CustomerDelete} /> */}
           <Route path="/show/:id" exact component={CustomerShow} />
           <Route path="/documentation" exact component={Documentation} />
           <Redirect to="/" />
