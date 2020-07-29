d3c2742c84c2d6cc4a7ed1f46e477efb18ac9ed7
apple juice <strbac.jelena.js@gmail.com>
2020-07-29 19:44:51 +0200

    Updates

---
+++ precache-manifest.acafc84679b51a44a4af165d969e595a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b711385952a59e3ea5ee3d4edbd5cf7c",
+    "url": "/customer-manager/index.html"
+  },
+  {
+    "revision": "bbcb127dd52d0538a47b",
+    "url": "/customer-manager/static/css/main.df48186d.chunk.css"
+  },
+  {
+    "revision": "bbb944993e9f5d659686",
+    "url": "/customer-manager/static/js/2.39cf71e3.chunk.js"
+  },
+  {
+    "revision": "c7385e80359cf5e0f3ca0810a73d38da",
+    "url": "/customer-manager/static/js/2.39cf71e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbcb127dd52d0538a47b",
+    "url": "/customer-manager/static/js/main.b966b883.chunk.js"
+  },
+  {
+    "revision": "ca3f3f195bd6c5fcad0d",
+    "url": "/customer-manager/static/js/runtime-main.966a8653.js"
+  }
+]);
